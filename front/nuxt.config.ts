f0dbaf14d442e4fca5465a912a4eb359d9f77d70
matthieu butelle <matthieu.butelle@thetribe.io>
2024-10-19 18:43:57 +0200

    [dev] add client dependencies

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,5 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: [
+        '@primevue/nuxt-module',
+        '@nuxt/test-utils/module'
+    ],
+    primevue: {
+        options: {
+            theme: {
+                preset: Aura
+            }
+        }
+    }
 })
